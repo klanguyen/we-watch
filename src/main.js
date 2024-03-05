1b9bfe498fd821e827f75d8266099d50f2b11553
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-03-05 11:55:57 -0600

    Added loading spinner & error handling for signin/signup process

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import TheHeader from './components/layout/TheHeader.vue';
 import TheFooter from './components/layout/TheFooter.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseCard from './components/ui/BaseCard.vue';
+import BaseDialog from "@/components/ui/BaseDialog.vue";
+import BaseSpinner from "@/components/ui/BaseSpinner.vue";
 
 
 const app = createApp(App)
@@ -20,5 +22,7 @@ app.component('the-header', TheHeader);
 app.component('the-footer', TheFooter);
 app.component('base-button', BaseButton);
 app.component('base-card', BaseCard);
+app.component('base-dialog', BaseDialog);
+app.component('base-spinner', BaseSpinner);
 
 app.mount('#app')
