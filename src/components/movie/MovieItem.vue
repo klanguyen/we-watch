5da4f371bc83ba0c7c5acc062027f159e24db1f1
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-03-29 23:10:39 -0500

    Changed the movie item style

--- src/components/movie/MovieItem.vue
+++ src/components/movie/MovieItem.vue
@@ -6,6 +6,7 @@ import TmdbAPI from "@/services/TmdbAPI.js";
 const router = useRouter();
 
 const posterUrl = ref('');
+const genres = ref([])
 
 const props = defineProps({
   movie: {
@@ -20,15 +21,36 @@ function toDetailsPage(id) {
 
 onMounted(() => {
   posterUrl.value = TmdbAPI.getPosterImageUrl(props.movie.poster_path);
+
+  if(props.movie.genre_ids !== undefined) {
+    TmdbAPI.getGenreName(props.movie.genre_ids)
+        .then(response => {
+          genres.value = response;
+        })
+        .catch(err => {
+          console.error('Failed to load genres', err);
+        });
+
+  } else {
+    (props.movie.genres).forEach(genre => {
+      genres.value.push(genre.name);
+    })
+  }
 })
+
 </script>
 
 <template>
-  <base-card @click="toDetailsPage(props.movie.id)">
-    <h4>{{ props.movie.title }}</h4>
-    <img :src="posterUrl" :alt="props.movie.title + ' poster'">
-    <p>{{ props.movie.overview }}</p>
-  </base-card>
+  <div class="w-64" @click="toDetailsPage(props.movie.id)">
+    <div class="bg-gray-300 w-full h-auto p-4 rounded-lg shadow-md">
+      <img :src="posterUrl" class="rounded-lg shadow mb-2" :alt="props.movie.title + ' poster'">
+      <h1 class="text-sm font-semibold text-gray-950 tracking-wide">{{ props.movie.title }}</h1>
+      <h2
+          class="text-xs text-gray-950 tracking-wide mb-2"
+      >{{ genres.join(', ') }} </h2>
+      <span class="my-1 px-4 py-1 text-sm rounded-lg bg-yellow-500 rating-badge shadow">{{ Math.round(props.movie.vote_average*10)/10 }}</span>
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- src/services/TmdbAPI.js
+++ src/services/TmdbAPI.js
@@ -16,7 +16,30 @@ export default {
     },
     getPosterImageUrl(posterPath) {
         let baseImgUrl = 'https://image.tmdb.org/t/p/';
-        return baseImgUrl.concat('w154', posterPath);
+        return baseImgUrl.concat('original', posterPath);
+    },
+    getGenreName(idList) {
+        // first, get the official genres list
+        let config = {
+            params: {
+                api_key: import.meta.env.VITE_TMDB_API_KEY,
+                language: 'en'
+            }
+        };
+        return API().get(`/genre/movie/list`, config)
+                    .then(response => {
+                        let genreList = response.data.genres;
+                        let resultGenres = [];
+                        (genreList).forEach(genre => {
+                            idList.forEach(id => {
+                                if(genre.id === id) {
+                                    resultGenres.push(genre.name)
+                                }
+                            })
+                        });
+
+                        return resultGenres;
+                    });
     },
     fetchSingleMovie(id) {
         let config = {
