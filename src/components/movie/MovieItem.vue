eccd7e9386ce00dddb2359b73135047235efbb70
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-04-04 16:11:06 -0500

    Changed the style of movie items

--- src/components/movie/MovieItem.vue
+++ src/components/movie/MovieItem.vue
@@ -1,10 +1,12 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import {ref, onMounted, computed} from 'vue';
 import { useRouter } from 'vue-router';
 import TmdbAPI from "@/services/TmdbAPI.js";
+import { FwbBadge } from 'flowbite-vue'
+import {useStore} from "vuex";
 
 const router = useRouter();
-
+const store = useStore();
 const posterUrl = ref('');
 const genres = ref([])
 
@@ -18,19 +20,51 @@ function toDetailsPage(id) {
   router.push(`/movie/${id}`);
 }
 
+function addWatchedMovie() {
+  store.dispatch('movieLists/addWatchedMovie', props.movie)
+}
+function removeWatchedMovie() {
+  store.dispatch('movieLists/removeWatchedMovie', parseInt(props.movie.id));
+}
+function addGottaWatchMovie() {
+  store.dispatch('movieLists/addGottaWatchMovie', props.movie)
+}
+function removeGottaWatchMovie() {
+  store.dispatch('movieLists/removeGottaWatchMovie', parseInt(props.movie.id));
+}
+
+// TODO: get this to work
+const watched = computed(() => {
+  return store.getters["movieLists/watched"];
+})
+
+const gottaWatched = computed(() => {
+  return store.getters["movieLists/gottaWatched"];
+})
+
 onMounted(() => {
+  store.dispatch('movieLists/getWatchedStatus', parseInt(props.key));
+  store.dispatch('movieLists/getGottaWatchStatus', parseInt(props.key));
+
   posterUrl.value = TmdbAPI.getPosterImageUrl(props.movie.poster_path);
+  if(posterUrl.value === 'https://image.tmdb.org/t/p/w780null') {
+    posterUrl.value = 'https://placehold.it/500x500';
+  }
 
-  if(props.movie.genre_ids !== undefined) {
-    TmdbAPI.getGenreName(props.movie.genre_ids)
-        .then(response => {
-          genres.value = response;
-        })
-        .catch(err => {
-          console.error('Failed to load genres', err);
-        });
-
-  } else {
+  if(props.movie.genre_ids) {
+    console.log(props.movie.genre_ids);
+    if(props.movie.genre_ids.length > 0) {
+      TmdbAPI.getGenreName(props.movie.genre_ids)
+          .then(response => {
+            genres.value = response;
+          })
+          .catch(err => {
+            console.error('Failed to load genres', err);
+          });
+    } else {
+      genres.value = [];
+    }
+  } else if(props.movie.genres) {
     (props.movie.genres).forEach(genre => {
       genres.value.push(genre.name);
     })
@@ -40,18 +74,45 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="w-48 mb-5 mr-4" @click="toDetailsPage(props.movie.id)">
-    <div class="bg-gray-300 w-full h-auto p-4 rounded-lg shadow-md">
-      <img :src="posterUrl" class="rounded-lg shadow mb-2" :alt="props.movie.title + ' poster'">
-      <h1 class="text-sm font-semibold text-gray-950 tracking-wide">{{ props.movie.title }}</h1>
-      <h2
-          class="text-xs text-gray-950 tracking-wide mb-2"
-      >{{ genres.join(', ') }} </h2>
-      <span class="my-1 px-4 py-1 text-sm rounded-lg bg-yellow-500 rating-badge shadow">{{ Math.round(props.movie.vote_average*10)/10 }}</span>
+  <div class="group/item w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-5 cursor-pointer">
+    <a @click="toDetailsPage(props.movie.id)" class="relative">
+      <div class="w-full h-full flex justify-end items-start opacity-0 hover:opacity-100 p-4 absolute">
+        <div class="border-[1px] border-white/20 bg-black/50 backdrop-blur-md rounded-lg flex justify-center h-6 w-6 p-4 mr-1">
+          <font-awesome-icon class="p-3 self-center text-gray-50 hover:text-red-500 text-lg" :icon="['fas', 'heart']" />
+        </div>
+        <div v-if="!watched" class="border-[1px] border-white/20 bg-black/50 backdrop-blur-md rounded-lg flex justify-center h-6 w-6 p-4 mr-1" @click="addWatchedMovie">
+          <font-awesome-icon class="p-3 self-center text-gray-50 hover:text-green-500 text-lg" :icon="['fas', 'eye']" />
+        </div>
+
+        <div v-else class="border-[1px] border-white/20 bg-black/50 backdrop-blur-md rounded-lg flex justify-center h-6 w-6 p-4 mr-1" @click="removeWatchedMovie">
+          <font-awesome-icon class="p-3 self-center text-green-500 text-lg" :icon="['fas', 'eye']" />
+        </div>
+        <div v-if="!gottaWatched" class="border-[1px] border-white/20 bg-black/50 backdrop-blur-md rounded-lg flex justify-center h-6 w-6 p-4" @click="addGottaWatchMovie">
+          <font-awesome-icon class="p-3 self-center text-gray-50 hover:text-yellow-200 text-lg" :icon="['fas', 'bookmark']" />
+        </div>
+        <div v-else class="border-[1px] border-white/20 bg-black/50 backdrop-blur-md rounded-lg flex justify-center h-6 w-6 p-4" @click="removeGottaWatchMovie">
+          <font-awesome-icon class="p-3 self-center text-yellow-200 text-lg" :icon="['fas', 'bookmark']" />
+        </div>
+      </div>
+      <img class="rounded-t-lg hover:brightness-50" :src="posterUrl" :alt="props.movie.title + ' poster'" />
+
+    </a>
+    <div class="p-5">
+      <a @click="toDetailsPage(props.movie.id)">
+        <h5 class="mb-2 text-lg font-bold tracking-wide text-gray-900 dark:text-white">{{ props.movie.title }}</h5>
+      </a>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ genres.length > 0 ? genres.join(', ') : 'No genre specified' }}</p>
+      <div class="flex">
+        <fwb-badge size="sm" type="yellow">
+          <template #icon>
+            <font-awesome-icon :icon="['fas', 'star']" class="mr-1 pb-0.5 w-4 h-4" />
+          </template>
+          {{ Math.round(props.movie.vote_average*10)/10 }}
+        </fwb-badge>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-
 </style>
