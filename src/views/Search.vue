ddf5bf6757ba5ecfbf5338285ae4b1f2b5c6aa27
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-05-08 16:14:34 -0500

    Got rid of genres

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -29,13 +29,13 @@ onMounted(() => {
   <article class="px-6 py-3">
     <h1 class="text-2xl font-semibold mb-3">Search</h1>
     <search-bar></search-bar>
-    <h2 class="text-xl font-semibold mb-3">Browse All</h2>
+<!--    <h2 class="text-xl font-semibold mb-3">Browse All</h2>
     <div class="flex flex-wrap">
       <div v-for="genre in genresList" class="bg-gray-50 border-[1px] border-zinc-500 backdrop-blur-md rounded-lg flex justify-center p-4 mr-4 mb-3 hover:brightness-90">
         <img class="h-8 w-8 mr-2" :src="'./src/assets/img/genres/' + genre.icon + '.png'">
         <span class="pt-1">{{ genre.result.name }}</span>
       </div>
-    </div>
+    </div>-->
   </article>
 </template>
 
